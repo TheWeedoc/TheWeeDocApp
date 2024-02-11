f792ca98cd2bda1410b1b46deddd6721f8e5f293
Nirmalnio <nirmalnio123@gmail.com>
2024-02-11 20:25:47 +0530

    Skipped At added in task/PMI/PME

--- src/Api/Mainclient.js
+++ src/Api/Mainclient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://theweedoc.info/api/",
+  baseURL: "https://theweedoc.live/api/",
 });
 
 function getCookie(cname) {
