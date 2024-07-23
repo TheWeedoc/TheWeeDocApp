ccb1a3aae068908bd0bf0ec92062d73809622397
Nirmalnio <nirmalnio123@gmail.com>
2024-07-23 23:16:55 +0530

    issues fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Markup from "./markup";
 import { useDispatch } from "react-redux";
 import { checkAuth } from "./store/Home/authReducer";
 import { useEffect } from "react";
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-FR08YTKBZM");
 
 function App() {
   const dispatch = useDispatch();
@@ -11,6 +13,10 @@ function App() {
   useEffect(() => {
     // Dispatch the checkAuth action when the component mounts
     dispatch(checkAuth());
+  }, []);
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
   }, []);
 
   return (
